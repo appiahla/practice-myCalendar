72c1e0344de05d1939f1f05671617eb939c41694
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-11-29 09:49:58 -0500

    added header

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title> 508 Group Project </title>
     <link rel = "stylesheet" type = "text/css" href = "login-signup-style.css">
+    <link rel = "stylesheet" type = "text/css" href = "navigation.css">
 </head>
 <body>
 <div id="day-view-container">
