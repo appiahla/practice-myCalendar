15754646198cf2e83160d56d94bdc64b71f7041a
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-11-29 01:41:18 -0500

    edit record page

--- sendLogin.php
+++ sendLogin.php
@@ -6,57 +6,47 @@
 </head>
   
 <body>
-    <center>
-        <?php
 
-    $link = mysqli_connect(
-        'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com',
-        'admin',
-        'databasegroup',
-        'groupMyCalendar',
-        '3306');
-    
-    // Check connection
-    if (!$link) {
+    <?php
+
+        $link = mysqli(
+            'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com',
+            'admin',
+            'databasegroup',
+            'groupMyCalendar',
+            '3306');
+        
+        // Check connection
+        if ($link->connect_error) {
+            die("Connection failed: " . $link->connect_error);
+        } 
+
             
-        //kill the connection
-        die("Connection failed:" .mysqli_connect_error());
-    }
-
-        //Conditional to check for if the submit button was pressed or not
-       // if(isset( $_POST['loginFormPost'])) {
-        
-            //Once the button is pressed -> insert into database
-        
             //Get the variables that will be inserted into the database
-                $v_num = $_POST['vnum'];
-        
-                $email = $_POST['email'];
-        
-                $password = $_POST['password'];
+            $v_num = $_POST['vnum'];
+            
+            $email = $_POST['email'];
+            
+            $password = $_POST['password'];
 
-        
-                //now insert them into the database
-                $sql = "INSERT INTO Student (`v_num`, `username`, `password`) 
-                        VALUES ('$v_num', '$email', '$password')";
-                
-                
-        if (mysqli_query($link, $sql)) {    
+            
+            //now insert them into the database
+            $sql = "INSERT INTO Student (`v_num`, `username`, `password`) 
+                    VALUES ('$v_num', '$email', '$password')";
+                    
+
+        if ($link->query($sql) === TRUE) {
+            echo "New record created successfully"; 
             echo "New record created successfully";
             echo "<h3>data stored in a database successfully." 
-                . " Please browse your localhost php my admin" 
-                . " to view the updated data</h3>"; 
-  
-            echo nl2br("\n$v_num\n $email\n $password\n ");
+                            . " Please browse your localhost php my admin" 
+                            . " to view the updated data</h3>"; 
         } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($link);
+        echo "Error: " . $sql . "<br>" . $link->error;
         }
-  
-    mysqli_close($link);
-   // }
 
-?>
-</center>
+        $link->close();
+    ?>
 </body>
   
 </html>
