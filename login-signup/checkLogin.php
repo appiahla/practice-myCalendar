ad10f0deb420601dc877eb37d34397128adccb7a
Lois Appiah <appiahl2@vcu.edu>
2021-11-30 15:14:32 -0500

    updating tasks for vnumber

--- login-signup/checkLogin.php
+++ login-signup/checkLogin.php
@@ -204,9 +204,9 @@ if (!$link) {
           $description = $_POST['Description'];
           $end = $_POST['End'];
 
-          // $temp = $_SESSION['v_num']']
+          $temp = $_SESSION['v_num'];
 
-          $sql_current_tasks = "CALL create_todays_tasks($_SESSION['v_num']);";
+          $sql_current_tasks = "CALL create_todays_tasks('$temp');";
 
           if ($result_current_tasks = mysqli_query($link,$sql_current_tasks)) {
               // Return the number of rows in result set
