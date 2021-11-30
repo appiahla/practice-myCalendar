a0dd95ad7f41ca79f6ea677f868a16b35693400c
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-11-30 13:49:32 -0500

    test if v num is stored

---
+++ login-signup/showAllAssignments.php
@@ -0,0 +1,25 @@
+<?php
+
+$db_host = 'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com';
+$db_user = 'admin';
+$db_pass = 'databasegroup';
+$db_name = 'groupMyCalendar';
+$db_port = '3306';
+
+$link = mysqli_connect("$db_host","$db_user","$db_pass","$db_name", "$db_port");
+
+
+// Check connection
+if (!$link) {
+        
+    //kill the connection
+    die("Connection failed:" .mysqli_connect_error());
+}
+
+
+    $edit_v = $_SESSION['v_num'];
+    echo($edit_v);
+
+mysqli_close($link);
+
+?>
