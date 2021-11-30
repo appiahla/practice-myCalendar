8be39b69be7cf95fb844284172253d96162afee9
Lois Appiah <appiahl2@vcu.edu>
2021-11-30 16:19:48 -0500

    add basic view to profile

---
+++ login-signup/profilePage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> 508 Group Project </title>
+    <link rel = "stylesheet" type = "text/css" href = "successSubmit.css">
+    <link rel = "stylesheet" href = "../navigation.css">
+
+    <nav class="nav-bar" >
+      <div style="display: flex; justify-content: space-between;">
+        <a id="home-pic" href="">myCalendar</a>
+          <ul class="list">
+            <li class="list-item">
+              <a href="https://team2-508database.herokuapp.com/login-signup/checkLogin.php">Home</a>
+            </li>
+            <li class="list-item">
+              <a a href="">
+                <button class="btn">+ Add</button>
+              </a>
+            </li>
+            <li class="list-item">
+              <a href="https://team2-508database.herokuapp.com/login-signup/profilePage.php">
+                <button class="btn">Profile</button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </head>
+<body>
+<div id="day-view-container">
+  <div id="input-field">
+    <div class="mytabs">
+
+    
+    </div>
+    <a href="">
+      <button class="btn">View All Academic Tasks</button>
+    </a>
+  </div>
+</div>
+</body>
+</html>
