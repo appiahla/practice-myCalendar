f64a1b142fb65d270091e8d6a0c88a3324f158c3
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-12-01 01:19:09 -0500

    css for test view all

--- login-signup/showAllTests.php
+++ login-signup/showAllTests.php
@@ -81,25 +81,25 @@
                             $assessmentT_material = $row_current_assessmentsT['material'];
                             $assessmentT_notes= $row_current_assessmentsT['notes'];
 
-                        echo nl2br("<h3> Test Title:</h3>  $assessmentT_title\n\n");
+                        echo nl2br("<h4> Test Title:</h4>  $assessmentT_title\n\n");
                         
-                        echo nl2br("<h3> Test Course:</h3>  $assessmentT_course\n\n");
+                        echo nl2br("<h4> Test Course:</h4>  $assessmentT_course\n\n");
                                     
                         if ($assessmentT_date != ''){
                                     
-                            echo nl2br("<h3> Test Date:</h3>  $assessmentT_date\n\n");
+                            echo nl2br("<h4> Test Date:</h4>  $assessmentT_date\n\n");
                                     
                         }
                 
                         if ($assessmentT_material != '') {
                 
-                            echo nl2br("<h3> Test Material:</h3>  $assessmentT_material\n\n");
+                            echo nl2br("<h4> Test Material:</h4>  $assessmentT_material\n\n");
 
                         }
                                 
                         if ($assessmentT_notes != ''){
                 
-                            echo nl2br("<h3> Test Notes:</h3>  $assessmentT_notes\n\n");
+                            echo nl2br("<h4> Test Notes:</h4>  $assessmentT_notes\n\n");
                 
                         }
 
