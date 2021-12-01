8bca57d10a6d09446f1eb19f2ec8a61b25b85044
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-12-01 18:47:10 -0500

    view all grades

--- login-signup/calcGPA.php
+++ login-signup/calcGPA.php
@@ -60,16 +60,16 @@
             //see if the v_num exists
             $sql =  "SELECT AVG(grade) as avg_grade, SUM(credit) as total_credit FROM Course WHERE v_number='$get_v'";
 
-            if ($courseG = mysqli_query($link,$sql)) {
+            if ($course = mysqli_query($link,$sql)) {
                 
                 // Return the number of rows in result set
-                $rowcount_course = mysqli_num_rows($courseG);
+                $rowcount_course = mysqli_num_rows($course);
                 
                 //if there is a row
-                if($rowcount_course > 0) {
+                if($rowcount_course> 0) {
 
                     // output data of each row
-                    while($row_current_course= $courseG->fetch_assoc()) {
+                    while($row_current_course= $course->fetch_assoc()) {
 
                         $course_total_grade = $row_current_course['avg_grade'];
                         $course_total_credit = $row_current_course['total_credit'];
@@ -111,52 +111,10 @@
                             echo nl2br("<div class='course'><h2 style='margin-bottom: -10px; text-align: center;'> Your Semester GPA is: 0.0!</h2>\n\n");
                         }
                     }
-                } 
-                
-                else {
-                echo "No Courses!";
-            }
-            // Free result set
-            mysqli_free_result($row_current_course);
-            }
-
-
-            //see if the v_num exists
-            $sql2 =  "SELECT * FROM Course WHERE v_number='$get_v'";
-
-            if ($course = mysqli_query($link,$sql2)) {
-                
-                // Return the number of rows in result set
-                $rowcount_course = mysqli_num_rows($course);
-                
-                //if there is a row
-                if($rowcount_course> 0) {
-
-                    // output data of each row
-                    while($row_current_course= $course->fetch_assoc()) {
-
-                        //save variables in the case of wanting to edit
-                        $course_crn = $row_current_course['crn'];
-                        $course_num = $row_current_course['course_num'];
-                        $course_name = $row_current_course['course_name'];
-                        $course_section = $row_current_course['section_num'];
-                        $course_grade = $row_current_course['grade'];
-                
-                    
-                    echo nl2br("<div class='course'><h2 style='margin-bottom: -10px;'> Course $course_name: </h2> <div id='grades'><h3>$course_grade</h3></div>\n\n");
-
-                    if($course_section==1){
-                        echo nl2br("<h4>$course_num-001 $course_crn</h4>\n\n");
-                    }else{
-                        echo nl2br("<h4>$course_num-$course_section $course_crn</h4>\n\n");
-                    }
-                }
-           
                     echo "<form action='./viewGrades.php' method='POST'>                         
-                         <button type='submit' class='btn' id='viewAll' style='position: relative;left: 30%; margin-top: 15px;'>Go Back To Grades</button>
+                        <button type='submit' class='btn' id='viewAll' style='position: relative;left: 30%; margin-top: 15px;'>Go Back To Grades</button>
                         </form>";
-                     echo "<br>";
-   
+                        echo "<br>";
                 } 
                 
                 else {
@@ -166,10 +124,6 @@
             mysqli_free_result($row_current_course);
             }
                     
-            else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($link);
-            }
-
         mysqli_close($link);
         ?>
     </div>
