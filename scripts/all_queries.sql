833f4da12df7ada96aa8a264e4d6b6e61bb405aa
Lois Appiah <appiahl2@vcu.edu>
2021-12-01 12:46:08 -0500

    renamed queries file

--- scripts/all_queries.sql
+++ scripts/all_queries.sql
@@ -236,3 +236,12 @@ SET `assessment_title`='$title',
 	`material`='$material',
 	`notes`='$notes'
 WHERE `assessment_type`='Test' AND `assessment_id`='$id' AND `assessment_v_number`='$get_v';
+
+-- show academic and personal tasks
+SELECT task_id, v_number, task_title, task_type, task_course, NULL as task_with, NULL as task_location, task_description, date_of, task_status, task_recurring, recurringMon, recurringTues, recurringWed, recurringThurs, recurringFri, recurringSat, recurringSun, start_date, end_date
+  FROM AcademicTask
+UNION 
+SELECT task_id, v_number, task_title, task_type, NULL as task_course, task_with, task_location, task_description, date_of, task_status, task_recurring, recurringMon, recurringTues, recurringWed, recurringThurs, recurringFri, recurringSat, recurringSun, start_date, end_date
+  FROM PersonalTask
+  
+  WHERE v_number='V00875392';
