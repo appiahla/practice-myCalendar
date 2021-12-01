c3a4f28659d6ddb9c9c4464be0bd99c4ad965b84
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-12-01 06:55:04 -0500

    display courses

--- login-signup/addItems.php
+++ login-signup/addItems.php
@@ -1,3 +1,43 @@
+<?php
+
+session_start();  
+
+    $db_host = 'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com';
+    $db_user = 'admin';
+    $db_pass = 'databasegroup';
+    $db_name = 'groupMyCalendar';
+    $db_port = '3306';
+
+    $link = mysqli_connect("$db_host","$db_user","$db_pass","$db_name", "$db_port");
+
+    // Check connection
+    if (!$link) {
+                
+    //kill the connection
+      die("Connection failed:" .mysqli_connect_error());
+    }
+
+    $get_v = $_SESSION['v_num'];
+
+    $sql = "SELECT course_name FROM Course WHERE v_number=$get_v";
+
+    if ($result = mysqli_query($link,$sql)) {
+      // Return the number of rows in result set
+      $rowcount_result = mysqli_num_rows($result);
+  
+      //if there is a row
+      if($rowcount_result > 0) { 
+
+          // output data of each row
+          while($row_current_result = $result->fetch_assoc()) {
+          
+            $result_name= $row_current_result['course_name'];
+            echo $result_name;
+          }
+      }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
