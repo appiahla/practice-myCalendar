2ee6a30267c5b5618a9b8e5c7bb00c26c7583b63
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-12-01 06:16:41 -0500

    blah

--- login-signup/addItems.php
+++ login-signup/addItems.php
@@ -1,22 +1,5 @@
 <?php
   session_start();  
-
-  $db_host = 'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com';
-        $db_user = 'admin';
-        $db_pass = 'databasegroup';
-        $db_name = 'groupMyCalendar';
-        $db_port = '3306';
-
-        $link = mysqli_connect("$db_host","$db_user","$db_pass","$db_name", "$db_port");
-
-        // Check connection
-        if (!$link) {
-                
-            //kill the connection
-            die("Connection failed:" .mysqli_connect_error());
-        }
-
-  $get_v = $_SESSION['v_num'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -72,17 +55,39 @@
                 <input  id="dueDate" type="date" name="DueDate"><br><br>
             </label>
               
-            <label for="courseName"> Course Name: </label>
-                <select name="CourseName">
-                    <?php 
-                    $sql = mysqli_query($link, "SELECT course_name FROM Course WHERE v_number=$get_v");
-                      while ($row = $sql->fetch_assoc()){
-                        $course_name_option=  $row['course_name'];
-                        echo "<option value=$course_name_option>" . $row['course_name'] . "</option>";
-                      }
-                    ?>
-                    </select><br><br>
-           
+            <label for="courseName">
+                Course Name:
+                <select name="CourseName" id="courseName" >
+                          <?php 
+                           $db_host = 'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com';
+                           $db_user = 'admin';
+                           $db_pass = 'databasegroup';
+                           $db_name = 'groupMyCalendar';
+                           $db_port = '3306';
+                   
+                           $link = mysqli_connect("$db_host","$db_user","$db_pass","$db_name", "$db_port");
+                   
+                           // Check connection
+                           if (!$link) {
+                                   
+                               //kill the connection
+                               die("Connection failed:" .mysqli_connect_error());
+                           }
+                   
+                          $get_v = $_SESSION['v_num'];
+
+                          $sql = mysqli_query($link, "SELECT course_name FROM Course WHERE v_number=$get_v");
+                              while ($row = $sql->fetch_assoc()){
+                                $row_course_name =  $row['course_name'];
+                                
+                                  echo "<option value=$row_course_name >" . $row['course_name'] . "</option>";
+                              }
+                          
+                            mysqli_close($link);
+                          ?>
+                          </select><br><br>
+                <br><br>
+            </label>
               
             <label for="assignmentDesc">
                 Description:
@@ -350,5 +355,3 @@
     </div>
   </body>
   </html>
-<?php
-mysqli_close($link);?>
