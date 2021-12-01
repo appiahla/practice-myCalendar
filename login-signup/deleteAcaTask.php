01d03c89da5a04e27be27d5ce40eab6f042e3fd7
Kendall McCleary <mcclearyke@mymail.vcu.edu>
2021-12-01 02:02:04 -0500

    delete for academic tasks

--- login-signup/deleteAcaTask.php
+++ login-signup/deleteAcaTask.php
@@ -1,5 +1,38 @@
 <?php
-  session_start();
+
+    session_start();
+    
+    $db_host = 'team2-database.cstfewbdata2.us-east-1.rds.amazonaws.com';
+    $db_user = 'admin';
+    $db_pass = 'databasegroup';
+    $db_name = 'groupMyCalendar';
+    $db_port = '3306';
+
+    $link = mysqli_connect("$db_host","$db_user","$db_pass","$db_name", "$db_port");
+
+    $delete_taskA_id = $_POST['taskA_id'];
+
+    // Check connection
+    if (!$link) {
+            
+        //kill the connection
+        die("Connection failed:" .mysqli_connect_error());
+    }
+
+        $get_v = $_SESSION['v_num'];
+
+        //see if the v_num exists
+        $sql =  "DELETE FROM AcademicTask WHERE task_id='$delete_taskA_id' AND assignment_v_number='$get_v'";
+
+        if (mysqli_query($link,$sql)) {
+            echo("Deleted");
+        }
+                
+        else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($link);
+        }
+
+    mysqli_close($link);
 ?>
 <!DOCTYPE html>
 <html>
